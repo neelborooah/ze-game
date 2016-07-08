ff24e5221f9f2c2d813f16b34569f44a13d6be8a
Neel <neelborooah@gmail.com>
2016-07-08 22:28:53 +0530

    installed express and setup dependency manager

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+/*
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, 'localhost');
 console.log('Server running at http://localhost:8080/');
+*/
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
